d0353658f3f8d5cb1df38092cdc5c1a4e8cfea67
athreya-harshith <athreya1207@gmail.com>
2024-02-14 19:45:05 +0530

    initial commit

---
+++ lesson-1/sandbox.ts
@@ -0,0 +1,8 @@
+const character = 'athreya';
+console.log(character)
+const inputs = document.querySelectorAll('input')// this selects all the input tags
+console.log(inputs)
+
+inputs.forEach(input =>{
+    console.log(input)
+})
