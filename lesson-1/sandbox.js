79b03571074812e6a9077db416ecc7c4063c1e64
athreya-harshith <athreya1207@gmail.com>
2024-02-14 22:59:37 +0530

    Explicit Types

--- lesson-1/sandbox.js
+++ lesson-1/sandbox.js
@@ -1,7 +1,54 @@
-var character = 'athreya';
-console.log(character);
-var inputs = document.querySelectorAll('input'); // this selects all the input tags
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// explicit types
+var character;
+var age;
+var isLoggedIn;
+// isLoggedIn='not' not possible
+isLoggedIn = true;
+character = 'athreya';
+age = 21;
+// Arrays 
+// let names : string[]
+// names.push('athreya')
+// console.log(names)
+// even though could be done in the above way , it gives us the type error as names is not yet initialized as
+// array but push() is being called 
+// either do this names = ['some values'] , then names.push('some other values') 
+// or do the below
+var names = [];
+names.push('alice');
+names.push('bob');
+console.log(names);
+var ages = [];
+ages.push(22, 2);
+console.log(ages);
+// names.push(2); gives error
+// Union types
+var mixed = [];
+mixed.push('athreya');
+mixed.push(21);
+mixed.push(true);
+console.log(mixed);
+var uid;
+uid = '123';
+console.log(uid);
+uid = 123;
+console.log(uid);
+//Objects
+var obj;
+obj = {
+    name: 'athreya',
+    work: 'intern',
+    age: 21
+};
+console.log(obj, 'object 1');
+var obj1;
+// obj1 = {
+//     name:123
+// } gives error
+obj1 = {
+    name: 'harshith',
+    work: 'intern',
+    age: 20
+};
+console.log(obj1);
+console.log('end');
--- lesson-1/sandbox.ts
+++ lesson-1/sandbox.ts
@@ -1,8 +1,75 @@
-const character = 'athreya';
-console.log(character)
-const inputs = document.querySelectorAll('input')// this selects all the input tags
-console.log(inputs)
+// explicit types
 
-inputs.forEach(input =>{
-    console.log(input)
-})
+let character : string
+let age : number
+let isLoggedIn:boolean
+
+// isLoggedIn='not' not possible
+isLoggedIn = true
+character = 'athreya'
+age = 21
+
+// Arrays 
+
+// let names : string[]
+// names.push('athreya')
+// console.log(names)
+// even though could be done in the above way , it gives us the type error as names is not yet initialized as
+// array but push() is being called 
+// either do this names = ['some values'] , then names.push('some other values') 
+// or do the below
+
+let names : string[] = []
+names.push('alice')
+names.push('bob')
+console.log(names)
+
+let ages : number[] = []
+ages.push(22,2)
+console.log(ages)
+
+// names.push(2); gives error
+
+// Union types
+
+let mixed : (string|boolean|number)[] = []
+mixed.push('athreya')
+mixed.push(21)
+mixed.push(true)
+console.log(mixed)
+
+let uid : number|string
+uid = '123'
+console.log(uid)
+uid = 123
+console.log(uid)
+
+//Objects
+
+let obj : object
+
+obj = {
+    name:'athreya',
+    work:'intern',
+    age :21
+}
+
+console.log(obj,'object 1')
+
+let obj1:{
+    name:string,
+    work:string,
+    age:number
+}
+
+// obj1 = {
+//     name:123
+// } gives error
+
+obj1 = {
+    name:'harshith',
+    work:'intern',
+    age:20
+}
+console.log(obj1)
+console.log('end')
