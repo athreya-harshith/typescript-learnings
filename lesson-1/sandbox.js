6ef5d600df2a9499048dbdbdae06c5276fa30b04
athreya-harshith <athreya1207@gmail.com>
2024-02-14 20:53:31 +0530

    Arrays and Objects

--- lesson-1/sandbox.js
+++ lesson-1/sandbox.js
@@ -1,7 +1,25 @@
-var character = 'athreya';
-console.log(character);
-var inputs = document.querySelectorAll('input'); // this selects all the input tags
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+// Arrays
+var names = ['athreya', 'harshith', 'alice', 'bob'];
+names.push('james'); // valid
+// names.push(29);// error
+var numbers = [19, 12, 12, 1, 11, 1, 223, 2];
+// numbers.push('alice') invalid
+numbers.push(200); // valid
+var mixed = ['alice', 23, 'bob', 10];
+mixed.push('hello');
+mixed.push(44);
+mixed[3] = 'athreya';
+// Objects
+var obj = {
+    name: 'athreya',
+    place: 'mysore',
+    age: 21
+};
+// obj.age ='hello' not possible
+// obj.skills = ['coding'] not possible
+obj = {
+    name: 'harshith',
+    place: 'mandya',
+    age: 20
+};
+// note that the structure after re-assigning the object must hold as well !!
--- lesson-1/sandbox.ts
+++ lesson-1/sandbox.ts
@@ -1,8 +1,33 @@
-const character = 'athreya';
-console.log(character)
-const inputs = document.querySelectorAll('input')// this selects all the input tags
-console.log(inputs)
+// Arrays
+let names = ['athreya','harshith','alice','bob']
+names.push('james')// valid
+// names.push(29);// error
 
-inputs.forEach(input =>{
-    console.log(input)
-})
+let numbers = [19,12,12,1,11,1,223,2]
+// numbers.push('alice') invalid
+numbers.push(200)// valid
+
+let mixed =['alice',23,'bob',10]
+mixed.push('hello')
+mixed.push(44)
+mixed[3]='athreya'
+
+// Objects
+
+let obj = 
+{
+    name:'athreya',
+    place:'mysore',
+    age :21
+}
+
+// obj.age ='hello' not possible
+
+// obj.skills = ['coding'] not possible
+
+obj = {
+    name:'harshith',
+    place:'mandya',
+    age :20
+}
+// note that the structure after re-assigning the object must hold as well !!
