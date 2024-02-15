db4718f857b431e77a64609337d2fa44cf2057c2
athreya-harshith <athreya1207@gmail.com>
2024-02-15 07:30:06 +0530

    Added tsconfig and better-workflow

--- lesson-1/sandbox.js
+++
@@ -1,7 +0,0 @@
-var character = 'athreya';
-console.log(character);
-var inputs = document.querySelectorAll('input'); // this selects all the input tags
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
--- lesson-1/sandbox.ts
+++
@@ -1,8 +0,0 @@
-const character = 'athreya';
-console.log(character)
-const inputs = document.querySelectorAll('input')// this selects all the input tags
-console.log(inputs)
-
-inputs.forEach(input =>{
-    console.log(input)
-})
