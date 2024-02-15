06eceff0ffd3159e10ec75a696d4e9dc5ad9d99e
athreya-harshith <athreya1207@gmail.com>
2024-02-15 07:52:37 +0530

    Type Aliases

---
+++ lesson-1/src/sandbox.ts
@@ -0,0 +1,7 @@
+type personObject = {name:string,uid:number|string}
+let greet = (obj : {name:string,uid:number|string})=>{
+    //some task
+}
+let person : {name:string,uid:number|string}
+
+let person2 : personObject
