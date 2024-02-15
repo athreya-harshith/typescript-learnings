903f54ff2c7220f24166a22324956e96636abf4e
athreya-harshith <athreya1207@gmail.com>
2024-02-15 07:09:17 +0530

    Any type

--- lesson-1/sandbox.js
+++ lesson-1/sandbox.js
@@ -1,7 +1,23 @@
-var character = 'athreya';
-console.log(character);
-var inputs = document.querySelectorAll('input'); // this selects all the input tags
-console.log(inputs);
-inputs.forEach(function (input) {
-    console.log(input);
-});
+var age = 25;
+// the any type can interchange its type in future
+age = true;
+console.log(true);
+age = 'hello';
+console.log(age);
+var mixed = [];
+mixed.push(5);
+mixed.push('athreya');
+mixed.push(false);
+console.log(mixed);
+var obj;
+obj = {
+    name: 'athreya',
+    age: 21
+};
+console.log(obj);
+obj = {
+    name: 24,
+    age: 'athreya'
+};
+console.log(obj);
+//  as seen above with the object obj , its more prone to mistakes , hence use this with caution
--- lesson-1/sandbox.ts
+++ lesson-1/sandbox.ts
@@ -1,8 +1,29 @@
-const character = 'athreya';
-console.log(character)
-const inputs = document.querySelectorAll('input')// this selects all the input tags
-console.log(inputs)
+let age : any = 25
+// the any type can interchange its type in future
+age = true
+console.log(true)
 
-inputs.forEach(input =>{
-    console.log(input)
-})
+age = 'hello'
+console.log(age)
+
+let mixed : any[] = []
+mixed.push(5)
+mixed.push('athreya')
+mixed.push(false)
+console.log(mixed)
+
+let obj : { name : any , age: any}
+
+obj = {
+    name:'athreya',
+    age : 21
+}
+
+console.log(obj)
+
+obj = {
+    name:24,
+    age:'athreya'
+}
+console.log(obj)
+//  as seen above with the object obj , its more prone to mistakes , hence use this with caution
